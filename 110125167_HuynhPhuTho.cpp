4e07472d05387976232940eab01da88662b6fc46
phutho12212345-blip <phutho12212345@gmail.com>
2026-01-28 07:48:39 +0700

    Add files via upload

---
+++ 110125167_HuynhPhuTho.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main()
+
+{
+	int a, b, t, h, s;
+	float x;
+	
+	printf("NHap a: =" );
+	scanf("%d", &a);
+	
+	printf("NHap b: =" );
+	scanf("%d", &b);
+	
+	printf("\nTong %d + %d =%d", a, b, t);
+	printf("\nHieu %d - %d =%d", a, b, h);
+	printf("\nTich %d * %d =%d", a, b, s);
+	printf("\nThuong %d / %d =%f", a, b, x);
+	
+	
+	
+	return 0;
+	}
